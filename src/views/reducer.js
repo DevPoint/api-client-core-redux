8206f94a58056afc361f09ab80e67cdc8d51063d
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-22 12:05:08 +0200

    added secure check for single VIEW actions

--- src/views/reducer.js
+++ src/views/reducer.js
@@ -22,37 +22,39 @@ export const defaultView = {
 
 const reducer = (state, action) => {
     const actionTypeFrags = action.type.split('_');
-    const updateView = { viewId: action.id };
-    if (action.payload) {
-        for (let propKey in action.payload) {
-            if (propKey === 'loadingMeta' || propKey === 'validationErrors') {
-                updateView[propKey] = assign({}, action.payload[propKey]);
-            }
-            else if (propKey === 'errors') {
-                updateView[propKey] = action.payload[propKey].slice(0));
-            }
-            else {
-                updateView[propKey] = action.payload[propKey];
+    if (actionTypeFrags[1] === 'VIEW') {
+        const updateView = { viewId: action.id };
+        if (action.payload) {
+            for (let propKey in action.payload) {
+                if (propKey === 'loadingMeta' || propKey === 'validationErrors') {
+                    updateView[propKey] = assign({}, action.payload[propKey]);
+                }
+                else if (propKey === 'errors') {
+                    updateView[propKey] = action.payload[propKey].slice(0));
+                }
+                else {
+                    updateView[propKey] = action.payload[propKey];
+                }
             }
         }
-    }
-    let updateState = null;
-    switch (actionTypeFrags[0]) {
-        case 'ADD':
-        case 'SET':
-            updateState = assign({}, state);
-            updateState[action.id] = assign({}, defaultView, updateView);
-            state = assign({}, state, updateState);
-            break;
-        case 'UPDATE':
-            updateState = assign({}, state);
-            updateState[action.id] = assign({}, state[action.id], updateView);
-            state = assign({}, state, updateState);
-            break;
-        case 'REMOVE':
-            state = assign({}, state);
-            delete state[action.id];
-            break;
+        let updateState = null;
+        switch (actionTypeFrags[0]) {
+            case 'ADD':
+            case 'SET':
+                updateState = assign({}, state);
+                updateState[action.id] = assign({}, defaultView, updateView);
+                state = assign({}, state, updateState);
+                break;
+            case 'UPDATE':
+                updateState = assign({}, state);
+                updateState[action.id] = assign({}, state[action.id], updateView);
+                state = assign({}, state, updateState);
+                break;
+            case 'REMOVE':
+                state = assign({}, state);
+                delete state[action.id];
+                break;
+        }
     }
     return state;
 };
